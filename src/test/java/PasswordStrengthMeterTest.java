ada0e4afe45d34ff3a8664b8c1f5a0c3f4af07fa
kaifazhehj99 <kaifazhehj99@gmail.com>
2024-05-22 15:39:32 +0900

    refactor

--- src/test/java/PasswordStrengthMeterTest.java
+++ src/test/java/PasswordStrengthMeterTest.java
@@ -11,31 +11,30 @@ public class PasswordStrengthMeterTest {
     // 2개의 규칙을 충족하면 암호는 보통이다.
     // 1개 이하의 규칙을 충족하면 암호는 약함이다.
 
+    private PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
+    private void assertStrength (String password, PasswordStrength expStr){
+        PasswordStrength result = meter.meter(password);
+        assertEquals(expStr,result);
+    }
+
     // 모든 규칙을 충족하는 경우
     @Test
     void meetsAllCriteria_Then_Strong(){
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("ab12!@AB");
-        assertEquals(PasswordStrength.STRONG,result);
-        PasswordStrength result2 =meter.meter("abc1!Add");
-        assertEquals(PasswordStrength.STRONG,result2);
+        assertStrength("ab12!@AB",PasswordStrength.STRONG);
+        assertStrength("abc1!Add",PasswordStrength.STRONG);
     }
-
 
     // 길이만 8글자 미만이고 나머지 조건은 충족하는 경우
     @Test
     void meetsOtherCriteria_except_for_Length_Then_Normal(){
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("ab12!@A");
-        assertEquals(PasswordStrength.NORMAL,result);
+        assertStrength("ab12!@A",PasswordStrength.NORMAL);
     }
 
     // 숫자를 포함하지 않고 나머지 조건은 충족하는 경우
     @Test
     void meetsOtherCriteria_except_for_number_Then_Normal(){
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("ab!@ABqwer");
-        assertEquals(PasswordStrength.NORMAL,result);
+        assertStrength("ab!@ABqwer",PasswordStrength.NORMAL);
     }
 
 }
