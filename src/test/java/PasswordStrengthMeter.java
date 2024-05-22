2d375e67f70eb897e3fc109bcb110f0a163a2afe
kaifazhehj99 <kaifazhehj99@gmail.com>
2024-05-22 19:32:55 +0900

    refactor

--- src/test/java/PasswordStrengthMeter.java
+++ src/test/java/PasswordStrengthMeter.java
@@ -5,20 +5,24 @@ public class PasswordStrengthMeter {
 
         if(s == null || s.isEmpty()) return PasswordStrength.INVALID;
 
-        int metCounts = 0;
-
-        boolean lenghthEnough =s.length() >= 8;
-        if(lenghthEnough) metCounts++;
-        boolean containsNum = meetsContainingNumberCriteria(s);
-        if(containsNum) metCounts++;
-        boolean containsUpp = meetsContainingUppercaseCriteria(s);
-        if(containsUpp) metCounts++;
+        int metCounts = getMetCriteriaCounts(s);
 
         if(metCounts <= 1) return PasswordStrength.WEAK;
         if(metCounts == 2) return PasswordStrength.NORMAL;
 
         return PasswordStrength.STRONG;
     }
+
+    private int getMetCriteriaCounts(String s){
+        int metCounts = 0;
+
+        if(s.length() >= 8) metCounts++;
+        if(meetsContainingNumberCriteria(s)) metCounts++;
+        if(meetsContainingUppercaseCriteria(s)) metCounts++;
+
+        return metCounts;
+    }
+
     private boolean meetsContainingNumberCriteria(String s){
         for (char ch : s.toCharArray()){
             if(ch >= '0' && ch <='9'){
