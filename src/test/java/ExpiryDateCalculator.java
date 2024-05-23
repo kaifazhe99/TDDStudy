39c2cad96f24235ff7aa77a5f275856a71f5a794
kaifazhehj99 <kaifazhehj99@gmail.com>
2024-05-23 10:39:04 +0900

    CHAPTER03_쉬운 것부터 테스트

---
+++ src/test/java/ExpiryDateCalculator.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class ExpiryDateCalculator {
+
+    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
+        return LocalDate.of(2019, 4, 1);
+    }
+}
---
+++ src/test/java/ExpiryDateCalculatorTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExpiryDateCalculatorTest {
+    // 서비스를 사용하려면 매달 1만 원을 선불로 납부한다. 납부일 기준으로 한 달 뒤가 서비스 만료일이 된다.
+    // 2개월 이상 요금을 납부랑 수 있다.
+    // 10만 원을 납부하면 서비스를 1년 제공한다.
+
+
+    @Test
+    void 만원_납부하면_한달_뒤가_만요일이_됨() {
+        LocalDate billingDate = LocalDate.of(2019, 3, 1);
+        int payAmount = 10_000;
+
+        ExpiryDateCalculator cal = new ExpiryDateCalculator();
+        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+
+        assertEquals(LocalDate.of(2019, 4, 1), expiryDate);
+
+    }
+
+}
